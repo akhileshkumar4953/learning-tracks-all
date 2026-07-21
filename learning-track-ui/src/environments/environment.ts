8a3a54d5cf3aa060884e21fb6e66fde7e75020b2
akhileshkumar4953 <45279792+akhileshkumar4953@users.noreply.github.com>
2026-07-21 19:01:18 +0530

    Update environment.ts

--- learning-track-ui/src/environments/environment.ts
+++ learning-track-ui/src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
 
   production: false,
 
-  apiUrl: 'http://localhost:8080/api'
+  // apiUrl: 'http://localhost:8080/api'
+    apiUrl: 'http://13.203.210.158:8080/api'
 
 };
